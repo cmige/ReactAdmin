264719e15b6b0ccd51a6062c11e4a8f1b1ad2447
cmige <1437744965@qq.com>
2021-05-09 21:31:33 +0800

    完善图表页面

--- src/pages/charts/children/pie.jsx
+++ src/pages/charts/children/pie.jsx
@@ -22,6 +22,8 @@ export default class Pie extends Component{
             {value: 919, name: '袜子'}
         ]
     }
+
+
     getOption = () => {
         return  {
             title: {
@@ -53,42 +55,30 @@ export default class Pie extends Component{
         };
     }
     getOption2 = () => {
-        return {
-            legend: {},
+        return  {
+            title: {
+                text: '库存',
+                left: 'center'
+            },
             tooltip: {
-                trigger: 'axis',
-                    showContent: false
+                trigger: 'item'
             },
-            dataset: {
-                source: [
-                    ['product', '2012', '2013', '2014', '2015', '2016', '2017'],
-                    ['Milk Tea', 56.5, 82.1, 88.7, 70.1, 53.4, 85.1],
-                    ['Matcha Latte', 51.1, 51.4, 55.1, 53.3, 73.8, 68.7],
-                    ['Cheese Cocoa', 40.1, 62.2, 69.5, 36.4, 45.2, 32.5],
-                    ['Walnut Brownie', 25.2, 37.1, 41.2, 18, 33.9, 49.1]
-                ]
+            legend: {
+                orient: 'vertical',
+                left: 'left',
             },
-            xAxis: {type: 'category'},
-            yAxis: {gridIndex: 0},
-            grid: {top:'55%'},
             series: [
-                {type: 'line', smooth: true, seriesLayoutBy: 'row', emphasis: {focus: 'series'}},
-                {type: 'line', smooth: true, seriesLayoutBy: 'row', emphasis: {focus: 'series'}},
-                {type: 'line', smooth: true, seriesLayoutBy: 'row', emphasis: {focus: 'series'}},
-                {type: 'line', smooth: true, seriesLayoutBy: 'row', emphasis: {focus: 'series'}},
                 {
+                    name: '访问来源',
                     type: 'pie',
-                    id: 'pie',
-                    radius: '30%',
-                    center: ['50%', '25%'],
-                    emphasis: {focus: 'data'},
-                    label: {
-                        formatter: '{b}: {@2012} ({d}%)'
-                    },
-                    encode: {
-                        itemName: 'product',
-                        value: '2012',
-                        tooltip: '2012'
+                    radius: '50%',
+                    data: this.state.stores,
+                    emphasis: {
+                        itemStyle: {
+                            shadowBlur: 10,
+                            shadowOffsetX: 0,
+                            shadowColor: 'rgba(0, 0, 0, 0.5)'
+                        }
                     }
                 }
             ]
@@ -97,13 +87,13 @@ export default class Pie extends Component{
     render() {
         return(
             <>
-                <Card >
-                    <Button type='primary' onClick={this.update}>更新</Button>
-                </Card>
+                {/*<Card >*/}
+                    {/*<Button type='primary' onClick={this.update}>更新</Button>*/}
+                {/*</Card>*/}
                 <Card className='pie'>
                     <ReactECharts option={this.getOption()}/>
                 </Card>
-                <Card className='pie' style={{height:500}}>
+                <Card className='pie'>
                     <ReactECharts option={this.getOption2()}/>
                 </Card>
             </>
